72dde7cc17d799c0a38b6725f861a02fedc8def3
Martin Disibio <mdisibio@gmail.com>
2023-09-28 23:05:40 -0400

    Automatically raise and lower desk on a timer, cli flags, scan mode

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"flag"
 	"fmt"
+	"time"
 
 	"tinygo.org/x/bluetooth"
 )
@@ -17,15 +19,15 @@ var (
 	commandLower       = []byte{241, 241, 2, 0, 2, 126}
 	commandGoToPreset1 = []byte{241, 241, 5, 0, 5, 126}
 	commandGoToPreset2 = []byte{241, 241, 6, 0, 6, 126}
-	commandStatus      = []byte{241, 241, 7, 0, 7, 126} // Notify current status including height with other unknowns
+	commandStatus      = []byte{241, 241, 7, 0, 7, 126} // Gets current height with other unknowns
 
 	// Other commands found from experimentation
-	// zero  = []byte{241, 241, 0, 0, 0, 126} // Wakes up the display but doesn't  do anything
-	// preset1Program = []byte{241, 241, 3, 0, 3, 126} // Reprogram preset 1
-	// preset2Program = []byte{241, 241, 4, 0, 4, 126} // Reprogram preset 2
-	// eight   = []byte{241, 241, 8, 0, 8, 126}   // Notify current status unknowns
-	// nine    = []byte{241, 241, 9, 0, 9, 126}   // Notify current status unknowns
-	// ten     = []byte{241, 241, 10, 0, 10, 126} // Wakes up the display but doesn't do anything
+	// []byte{241, 241, 0, 0, 0, 126} 	// Wakes up the display but doesn't  do anything
+	// []byte{241, 241, 3, 0, 3, 126} 	// Reprogram preset 1 to current desk height
+	// []byte{241, 241, 4, 0, 4, 126} 	// Reprogram preset 2 to current desk height
+	// []byte{241, 241, 8, 0, 8, 126}   // Returns unknown data
+	// []byte{241, 241, 9, 0, 9, 126}   // Returns unknown data
+	// []byte{241, 241, 10, 0, 10, 126} // Wakes up the display but doesn't do anything
 )
 
 type desk struct {
@@ -46,161 +48,218 @@ type desk struct {
 	height float32 // in inches
 }
 
-func main() {
-	deviceAddr := &bluetooth.Address{}
-	deviceAddr.Set("20e46da8-f165-8fbd-39a4-81cb8a91f664")
+type config struct {
+	scan          bool
+	deviceAddress string
+	preset1Time   time.Duration
+	preset2Time   time.Duration
+}
 
-	// Enable BLE interface.
+func (c *config) registerFlagsAndApplyDefaults() {
+	flag.BoolVar(&c.scan, "scan", false, "Scan and print nearby devices then exit")
+	flag.StringVar(&c.deviceAddress, "device", "", "Chosen device address")
+	flag.DurationVar(&c.preset1Time, "preset1", 15*time.Minute, "Time to stay in preset1 position")
+	flag.DurationVar(&c.preset2Time, "preset2", 15*time.Minute, "Time to stay in preset2 position")
+}
+
+func main() {
+	c := &config{}
+	c.registerFlagsAndApplyDefaults()
+	flag.Parse()
+
+	if !c.scan && c.deviceAddress == "" {
+		fmt.Println("Must specify one of --scan or --device")
+		return
+	}
+
 	err := adapter.Enable()
 	if err != nil {
 		panic(err)
 	}
 
-	// devices := map[string]bluetooth.ScanResult{}
+	if c.scan {
+		scan()
+		return
+	}
 
-	// Start scanning.
-	/*println("scanning...")
-	err = adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-		// addr := device.Address.String()
-		// if prior, ok := devices[addr]; !ok || !bytes.Equal(device.AdvertisementPayload.Bytes(), prior.AdvertisementPayload.Bytes()) {
-		//	devices[addr] = device
-		if device.Address == *deviceAddr && device.LocalName() != "" {
-			fmt.Println("found device:", "Address:", device.Address.String(), "RSSI:", device.RSSI, "LocalName", device.LocalName(), device)
+	deviceAddr := &bluetooth.Address{}
+	deviceAddr.Set(c.deviceAddress)
 
-			if device.HasServiceUUID(serviceuuid) {
-				fmt.Println("Has service!")
-			}
-
-			d, err := adapter.Connect(device.Address, bluetooth.ConnectionParams{})
-			if err != nil {
-				fmt.Println("Error connecting to device", err)
-				return
-			}
-
-		}
-
-
-
-		services, err := device.DiscoverServices(nil)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println("  Found services:")
-		for _, s := range services {
-			fmt.Println("  ", s.String())
-		}*/
-
-	desk := desk{}
-
-	fmt.Println("Connecting")
-	device, err := adapter.Connect(*deviceAddr, bluetooth.ConnectionParams{})
+	fmt.Println("Connecting to:", deviceAddr)
+	desk, err := connect(*deviceAddr)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected")
-	desk.address = *deviceAddr
+	fmt.Println("Connected to desk:")
+	fmt.Println("  Model       ", desk.model)
+	fmt.Println("  Manufacturer", desk.manufacturer)
+	fmt.Println("  SerialNumber", desk.serialNumber)
+	fmt.Println("  HardwareRev ", desk.hardwareRev)
+	fmt.Println("  SoftwareRev ", desk.softwareRev)
+	fmt.Println("  FirmwareRev ", desk.firmwareRev)
 
-	fmt.Println("Connected")
+	listenAndControl(desk, c)
+}
+
+func scan() error {
+	fmt.Println("Scanning for nearby devices.  Press ctrl-c to exit...")
+	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+		if device.HasServiceUUID(upliftService) {
+			fmt.Println("Found Uplift desk:", "address=", device.Address.String(), "name=", device.LocalName())
+		}
+	})
+	return err
+}
+
+func connect(address bluetooth.Address) (*desk, error) {
+	desk := &desk{
+		address: address,
+	}
+
+	device, err := adapter.Connect(address, bluetooth.ConnectionParams{})
+	if err != nil {
+		return nil, err
+	}
+
 	services, err := device.DiscoverServices([]bluetooth.UUID{upliftService, bluetooth.ServiceUUIDDeviceInformation})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// Uplift desk service
-	chars, err := services[0].DiscoverCharacteristics(nil)
+	chars, err := services[0].DiscoverCharacteristics(nil) //[]bluetooth.UUID{command, response})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+
 	for _, char := range chars {
 		char := char
 		switch char.UUID() {
 		case command:
-			desk.command = &chars[0]
-
+			desk.command = &char
 		case response:
-			desk.response = &chars[1]
-			desk.response.EnableNotifications(func(buf []byte) {
-				if handleResponse(buf, &desk) {
-					fmt.Println("Desk height:", desk.height)
-				}
-			})
-
+			desk.response = &char
 		default:
-			fmt.Println("Found characteristic: ", char.String(), "Value:", readCharacteristic(char))
+			x, err := readCharacteristic(char)
+			fmt.Println("Unknown characteristic", char.UUID(), x, err)
+			char.EnableNotifications(func(buf []byte) {
+				fmt.Println("Got bytes for char", char.UUID(), buf)
+			})
 		}
 	}
 
 	// Device information service
 	chars, err = services[1].DiscoverCharacteristics(nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	for _, char := range chars {
-		// char := char
 
+	for _, char := range chars {
 		switch char.UUID() {
 		case bluetooth.CharacteristicUUIDManufacturerNameString:
-			desk.manufacturer = readCharacteristic(char)
+			desk.manufacturer, _ = readCharacteristic(char)
 
 		case bluetooth.CharacteristicUUIDHardwareRevisionString:
-			desk.hardwareRev = readCharacteristic(char)
+			desk.hardwareRev, _ = readCharacteristic(char)
 
 		case bluetooth.CharacteristicUUIDModelNumberString:
-			desk.model = readCharacteristic(char)
+			desk.model, _ = readCharacteristic(char)
 
 		case bluetooth.CharacteristicUUIDSerialNumberString:
-			desk.serialNumber = readCharacteristic(char)
+			desk.serialNumber, _ = readCharacteristic(char)
 
 		case bluetooth.CharacteristicUUIDSoftwareRevisionString:
-			desk.softwareRev = readCharacteristic(char)
+			desk.softwareRev, _ = readCharacteristic(char)
 
 		case bluetooth.CharacteristicUUIDFirmwareRevisionString:
-			desk.firmwareRev = readCharacteristic(char)
+			desk.firmwareRev, _ = readCharacteristic(char)
 		}
 	}
 
-	fmt.Println("Listening to desk:", desk)
+	return desk, nil
+}
 
-	fmt.Println("Press ctrl+c to exit or enter new height and press enter:")
-	/*c := make(chan os.Signal, 1)
-	<-c*/
+func listenAndControl(desk *desk, c *config) error {
+	err := desk.response.EnableNotifications(func(buf []byte) {
+		if handleResponse(buf, desk) {
+			fmt.Println("Desk height:", desk.height)
+		}
+	})
+	if err != nil {
+		return nil
+	}
+
+	var (
+		stdin        = readStdIn()
+		dur, nextDur = c.preset1Time, c.preset2Time
+		t            = time.NewTimer(dur)
+	)
+
+	// fmt.Println("Going to preset 1 for", dur)
+	// desk.command.WriteWithoutResponse(commandGoToPreset1)
+
+	fmt.Println("Press ctrl+c to exit or enter command and press enter:")
+	fmt.Println("Commands:")
+	fmt.Println("  1 - go to preset 1")
+	fmt.Println("  2 - go to preset 2")
+	fmt.Println("  r - raise the desk a bit")
+	fmt.Println("  l - lower the desk a bit")
 	for {
-		if desk.command == nil {
-			continue
-		}
-		var command string
-		_, err := fmt.Scanln(&command)
-		if err != nil {
-			fmt.Println("Error scanning input", err)
-			continue
-		}
-		switch command {
-		case "1":
-			desk.command.WriteWithoutResponse(commandGoToPreset1)
-		case "2":
-			desk.command.WriteWithoutResponse(commandGoToPreset2)
-		case "s":
-			desk.command.WriteWithoutResponse(commandStatus)
-		case "r":
-			desk.command.WriteWithoutResponse(commandRaise)
-		case "l":
-			desk.command.WriteWithoutResponse(commandLower)
-		default:
-			fmt.Println("Unknown command: '", command, "'")
+		select {
+		case <-t.C:
+			switch dur {
+			case c.preset1Time:
+				fmt.Println("Going to preset 2 for", nextDur)
+				desk.command.WriteWithoutResponse(commandGoToPreset2)
+			case c.preset2Time:
+				fmt.Println("Going to preset 1 for", nextDur)
+				desk.command.WriteWithoutResponse(commandGoToPreset1)
+			}
+			dur, nextDur = nextDur, dur
+			t.Reset(dur)
+
+		case command := <-stdin:
+			switch command {
+			case "1":
+				desk.command.WriteWithoutResponse(commandGoToPreset1)
+			case "2":
+				desk.command.WriteWithoutResponse(commandGoToPreset2)
+			case "s":
+				desk.command.WriteWithoutResponse(commandStatus)
+			case "r":
+				desk.command.WriteWithoutResponse(commandRaise)
+			case "l":
+				desk.command.WriteWithoutResponse(commandLower)
+			default:
+				fmt.Println("Unknown command: '", command, "'")
+			}
 		}
 	}
 }
 
-func readCharacteristic(char bluetooth.DeviceCharacteristic) string {
+func readStdIn() <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		for {
+			var command string
+			_, err := fmt.Scanln(&command)
+			if err == nil {
+				ch <- command
+			}
+		}
+	}()
+	return ch
+}
+
+func readCharacteristic(char bluetooth.DeviceCharacteristic) (string, error) {
 	buf := make([]byte, 1024)
 	n, err := char.Read(buf)
 	if err != nil {
-		fmt.Println("error reading:", err)
+		return "", err
 	}
 	buf = buf[:n]
-	return string(buf)
+	return string(buf), nil
 }
 
 func handleResponse(buf []byte, desk *desk) (wasHeightChange bool) {
@@ -219,7 +278,7 @@ func handleResponse(buf []byte, desk *desk) (wasHeightChange bool) {
 	// 40.7 [242 242 1 3 1 151 7 163 126]
 
 	// When we send command 7 to get the status the
-	// packet comes back with one less leading bytes.
+	// packet comes back with one less leading byte.
 	// No idea why
 	//               |   |
 	//               v   v
@@ -234,10 +293,11 @@ func handleResponse(buf []byte, desk *desk) (wasHeightChange bool) {
 
 	switch {
 	case buf[0] == 1 && buf[1] == 3:
-		// Desk height
-		height := binary.BigEndian.Uint16(buf[2:4])
-		desk.height = float32(height) / 10
-		return true
+		// 1,3 means desk height notification
+		h := float32(binary.BigEndian.Uint16(buf[2:4])) / 10
+		changed := desk.height != h
+		desk.height = h
+		return changed
 	}
 
 	return false
